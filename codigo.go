29a529659c74355afb9e0694f9c3cdeee0ae5d25
AlexZama-San <61293885+AlexZama-San@users.noreply.github.com>
2020-12-23 19:37:36 -0600

    mas algoritmos

--- codigo.go
+++ codigo.go
@@ -31,8 +31,36 @@ func divalg(a int,b int) int{
 	for i=0;c>0;i++{
 		c=c-b
 	}
-	fmt.Println(i)
+	if(c<0){
+		i--
+	}
 	return i
+}
+func powalg(a int,b int) int {
+	var a2 int
+	a2=a
+	for c:=1;c<b;c++{
+		a=a*a2
+	}
+	return a
+}
+func factoalg(a int) int{
+	value:=1
+	for c:=1;c<=a;c++{
+		value=value*c
+	}
+	fmt.Println(value)
+	return value
+}
+func primealg(a int) bool{
+	var prime bool
+	prime=true
+	for i:=2;i<=a/2 && prime==true;i++{
+		if (a%i==0){
+			prime=false
+		}
+	}
+	return prime
 }
 
 func main(){
@@ -41,5 +69,20 @@ func main(){
 	fmt.Scanln(&b)
 	fmt.Println(a,"-",b,"=",subalg(a,b))
 	fmt.Println(a,"*",b,"=",multalg(a,b))
-	divalg(a,b)
+	fmt.Println(a,"/",b,"=",divalg(a,b))
+	fmt.Println(a,"^",b,"=",powalg(a,b))
+	fmt.Println("!",a,"=",factoalg(a))
+	fmt.Println("!",b,"=",factoalg(b))
+	primoA:=primealg(a)
+	primoB:=primealg(b)
+	if (primoA==true) {
+		fmt.Println(a,"es primo")
+	}else{
+		fmt.Println(a,"no es primo")
+	}
+	if (primoB==true) {
+		fmt.Println(b,"es primo")
+	}else{
+		fmt.Println(b,"no es primo")
+	}
 }
